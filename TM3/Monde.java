9935c7b8f3dc97526b25bf28681c7720b2034d0b
RenardFestif <saadi_juba@yahoo.fr>
2017-10-01 16:41:36 -0100

    ajout 2 methode

--- TM3/Monde.java
+++ TM3/Monde.java
@@ -22,17 +22,29 @@ public class Monde{
 		}
 	    }
 	}
-
-	public int nbVoisins(int nuLign, int nuCol){
-
-	    int nb = 0;
-	    for(int x=nuLign-1;x<nuLign+1;x++){
-		for (int y=nuCol; y<nuCol+1; y++){
-		    
-	    
-	    
-	}
     }
+
+    public int nbVoisins(int nuLign, int nuCol){
+	
+	int x2;
+	int y2;
+	
+	int nb = 0;
+	for(int x=nuLign-1;x<nuLign+1;x++){
+	    for (int y=nuCol; y<nuCol+1; y++){
+		x2=(x+dimx)%dimx;
+		y2=(y+dimy)%dimy;
+		if (tabCells[x2][y2])
+		    nb++;
+	     	       
+	
+	    }
+	}
+
+	return nb;
+	
+    }    
+    
 
     public String toString(){
 	String s = "Generation "+ noGener+"\n";
@@ -48,6 +60,31 @@ public class Monde{
 	}
 	return s;
     }
+
+
+    public void genSuiv(){
+	this.noGener++;
+
+	for (int y=0; y<dimy; y++){
+	    for (int x=0; x<dimx; x++){
+		
+		if (tabCells[x][y] == true && this.nbVoisins(x,y) < 2 ){
+		    tabCells[x][y] = false;
+		}
+		else if (tabCells[x][y] == true && this.nbVoisins(x,y) > 3){
+		    tabCells[x][y] = false;
+		}
+		else if ( tabCells[x][y] == false && this.nbVoisins(x,y) == 3){
+		    tabCells[x][y] = true;
+		}
+		
+	    }
+	}
+    }
+
+	
+	    
+			 
 }
 
 	     
