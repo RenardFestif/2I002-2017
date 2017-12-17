33b9541f074a99a19e34effe5e3a0d1fa6830572
Jörmungandr <Jrmungandr@PCMasterRace>
2017-12-17 17:29:19 +0100

    17/12/2017 17:30

--- MoteurRecherche/src/interrogation/VectorielCartesien.java
+++ MoteurRecherche/src/interrogation/VectorielCartesien.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interrogation;
 
 import java.util.HashMap;
 import indexation.Index;
 
 
-
-/**
- *
- * @author Jörmungandr
- */
 public class VectorielCartesien extends ModelIR {
     
     private Weighter w;
